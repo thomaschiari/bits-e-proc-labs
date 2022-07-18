16119e6f02020477ab9d5054ecb650f403b712a6
Rafael Corsi <corsiferrao@gmail.com>
2022-07-18 20:55:58 -0300

    fix toplevel.py

--- toplevel.py
+++ toplevel.py
@@ -23,17 +23,17 @@ def toplevel(LEDR, SW, KEY, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, CLOCK_50, RESET_
     # ---------------------------------------- #
     # ula
     # ---------------------------------------- #
-    ic1 = adder(sw_s[0:4], sw_s[6:10], ledr_s[0:4], ledr_s[9])
+    # ic1 = adder(sw_s[0:4], sw_s[6:10], ledr_s[0:4], ledr_s[9])
 
-    ledr_unsigned = ConcatSignal(*reversed(ledr_s))
-    ic2 = bin2hex(HEX0, ledr_unsigned)
+    # ledr_unsigned = ConcatSignal(*reversed(ledr_s))
+    # ic2 = bin2hex(HEX0, ledr_unsigned)
 
     # ---------------------------------------- #
     # seq
     # ---------------------------------------- #
-    # ic1 = blinkLedAdder(ledr_s[0], 100, CLOCK_50, RESET_N)
-    # ic2 = blinkLedAdder(ledr_s[1], 50, CLOCK_50, RESET_N)
-    # ic3 = blinkLedAdder(ledr_s[2], 1000, CLOCK_50, RESET_N)
+    ic1 = blinkLed(ledr_s[0], 100, CLOCK_50, RESET_N)
+    ic2 = blinkLed(ledr_s[1], 50, CLOCK_50, RESET_N)
+    ic3 = blinkLed(ledr_s[2], 1000, CLOCK_50, RESET_N)
 
     # ---------------------------------------- #
     @always_comb
@@ -65,3 +65,4 @@ CLOCK_50 = Signal(bool())
 RESET_N = ResetSignal(0, active=0, isasync=True)
 
 top = toplevel(LEDR, SW, KEY, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, CLOCK_50, RESET_N)
+top.convert(hdl="VHDL")
